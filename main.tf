23c7722719e2939abd00f062731b2244df447b81
Devadharshini M <jencydeva165@gmail.com>
2025-07-18 11:26:22 +0530

    message

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "user" {
+    source = "./modules/iam"
+    dfhgjhkjjk = "jesnasnandev"
+      
+}
+module "user1" {
+    source = "./modules/s3"
+  
+}
+module "user2" {
+    source = "./modules/ec2"
+  
+}
+
