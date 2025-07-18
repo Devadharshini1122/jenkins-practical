7c04eba94f2377510f64ad75867c73c31d882b22
Devadharshini M <jencydeva165@gmail.com>
2025-07-18 11:34:45 +0530

    message

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-module "user" {
-    source = "./modules/iam"
-    dfhgjhkjjk = "jesnasnandev"
-      
-}
-module "user1" {
-    source = "./modules/s3"
-  
-}
-module "user2" {
-    source = "./modules/ec2"
+provider "aws" {
+    region = "us-east-2"
   
 }
 
